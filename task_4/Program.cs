0470b4d1ddf3ce33188fee69d46b1a28cefb11b1
buzum <buzum@mail.ru>
2023-03-01 17:42:56 +0400

    First task for HW 1 with gitignore

---
+++ task_4/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите первое целое число: ");
+int user_number_one = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите второе целое число: ");
+int user_number_two = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите третье целое число: ");
+int user_number_three = int.Parse(Console.ReadLine() ?? "");
+int maximum = user_number_one;
+
+///////////////
+
+if (maximum < user_number_two)
+{
+    maximum = user_number_two;
+}
+if (user_number_three > maximum)
+{
+    maximum = user_number_three;
+}
+
+Console.WriteLine($"max = {maximum}");
+
