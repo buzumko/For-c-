cb4f63ad2f21583a9bf5d460817a63c226cd193d
buzum <buzum@mail.ru>
2023-03-01 17:53:42 +0400

    Third task for HW 1

---
+++ task_6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным 
+Console.Write("Введите целое число: ");
+int user_number_one = int.Parse(Console.ReadLine() ?? "");
+
+///////////////
+
+if (user_number_one % 2 == 0)
+{
+    Console.WriteLine($"число {user_number_one} четное");
+}
+else
+{
+    Console.WriteLine($"число {user_number_one} нечетное");
+}
